8e69295b718ceb1357fcdc75afac6055f576765a
rady <rady.agamy@egyptcloud.com>
2014-07-09 06:24:14 +0300

    refactoring

---
+++ RavenDbTest/Controllers/MedicineController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Raven.Client;
+using RavenDbTest.Helpers;
+using RavenDbTest.Models;
+
+namespace RavenDbTest.Controllers
+{
+    public class MedicineController : RavenDbController
+    {
+        public ActionResult Index()
+        {
+            return List();
+        }
+
+        public ActionResult AddNew()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNew(Item item)
+        {
+            Session.Store(item);
+            return Json(true);
+        }
+
+        public ActionResult List(int pageNumber=0,int pageSize=5)
+        {
+            var items = Session.Query<Item>().Skip(pageNumber*pageSize).Take(pageSize).ToList();
+            return Json(items);
+        }
+
+
+        public ActionResult Search(string @by="name", string criteria="")
+        {
+            List<Item> items;
+            switch (@by.ToLower())
+            {
+                case "cat":
+                    items = Session.Query<Item>().Where(x => x.Category == criteria).ToList();
+                    break;
+                case "desc":
+                    items = Session.Query<Item, ItemByDesc>().Search(x => x.Desc, criteria).ToList();
+                    break;
+                default:
+                    items = Session.Query<Item, ItemByName>().Search(x => x.Name, criteria)
+                        .ToList();
+                    break;
+            }
+            return Json(items);
+        }
+    }
+}
--- RavenDbTest/Models/Item.cs
+++ RavenDbTest/Models/Item.cs
@@ -12,7 +12,7 @@
     //public enum Category
     //{
     //    BodyTemperature,
-    //    Antipiotic,
+    //    Antibiotic,
     //    BodySkin
     //}
 }
